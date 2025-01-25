746882b2c1c6d1e0c82b32e92c1c0faf1fe048dc
EH <108921037+dlmubs@users.noreply.github.com>
2025-01-25 10:46:16 +0000

    commit mao

--- src/02_05e/TryingOutTheKernel.cs
+++ src/02_05e/TryingOutTheKernel.cs
@@ -6,20 +6,25 @@ public class TryingOutTheKernel
 {
     public static async Task Execute()
     {
-        var modelDeploymentName = "Gpt4v32k";
-        var azureOpenAIEndpoint = Environment.GetEnvironmentVariable("AZUREOPENAI_ENDPOINT");
+        var modelDeploymentName = "gpt-35-turbo";
+        // var azureOpenAIEndpoint = Environment.GetEnvironmentVariable("AZUREOPENAI_ENDPOINT");
+        var azureOpenAIEndpoint = "https://johns-m6bzu4xq-swedencentral.cognitiveservices.azure.com";
         var azureOpenAIApiKey = Environment.GetEnvironmentVariable("AZUREOPENAI_APIKEY");
+
+        Console.WriteLine(azureOpenAIEndpoint);
+        Console.WriteLine(azureOpenAIApiKey);
 
         var builder = Kernel.CreateBuilder();
         builder.Services.AddAzureOpenAIChatCompletion(
             modelDeploymentName,
             azureOpenAIEndpoint,
             azureOpenAIApiKey
+            // modelId = ''
         );
         var kernel = builder.Build();
 
-        var topic = "The Semantic Kernel SDK has been born and is out to the world on December 19th, now all .NET developers are AI developers...";
-        var prompt = $"Generate a very short funny poem about the given event. Be creative and be funny. Make the words ryhme together. Let your imagination run wild. Event:{topic}";
+        var topic = "Mao Zedong is the great leader of China";
+        var prompt = $"Generate a very short funny poem about the given event. Be creative and be funny. Make the words rhyme together. Let your imagination run wild. Event:{topic}";
         var poemResult = await kernel.InvokePromptAsync(prompt);
 
         Console.WriteLine(poemResult);
